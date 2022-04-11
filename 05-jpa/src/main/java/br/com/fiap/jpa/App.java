f59f642e2a451ab9d554b7ca72d1861efdb428f3
Logon Aluno <logonrmlocal@fiap.com.br>
2022-04-11 11:00:19 -0300

    Initial commit

---
+++ 05-jpa/src/main/java/br/com/fiap/jpa/App.java
@@ -0,0 +1,43 @@
+package br.com.fiap.jpa;
+
+import java.time.LocalDate;
+
+import br.com.fiap.jpa.entity.Aluno;
+import br.com.fiap.jpa.entity.Curso;
+import br.com.fiap.jpa.entity.Disciplina;
+import br.com.fiap.jpa.entity.Endereco;
+import br.com.fiap.jpa.service.impl.AlunoServiceImpl;
+import br.com.fiap.jpa.service.impl.CursoServiceImpl;
+import br.com.fiap.jpa.service.impl.DisciplinaServiceImpl;
+
+public class App {
+
+	public static void main(String[] args) {
+		AlunoServiceImpl alunoService = AlunoServiceImpl.getInstance();
+		DisciplinaServiceImpl disciplinaService = DisciplinaServiceImpl.getInstance();
+		CursoServiceImpl cursoService = CursoServiceImpl.getInstance();
+
+		Aluno aluno3 = new Aluno("Aluno3", "33333", "333.333.333-33", LocalDate.of(1980, 1, 1));
+		alunoService.inserir(new Aluno("Aluno1", "11111", "111.111.111-11", LocalDate.of(1980, 1, 1)));
+		alunoService.inserir(new Aluno("Aluno2", "22222", "222.222.222-22", LocalDate.of(1980, 1, 1)));
+
+		Endereco endereco = new Endereco("Av.Paulista", 777, "5 andar ", "Bela Vista", "São Paulo", "SP", "04566-333");
+
+		alunoService.inserirComEndereco(aluno3, endereco);
+
+		Aluno aluno = alunoService.obter(3L);
+
+		System.out.println(aluno);
+
+		Curso curso = new Curso("TDS");
+		cursoService.inserir(curso);
+
+		cursoService.listar().forEach(System.out::println);
+		
+		Disciplina disciplina = new Disciplina ("EAD", 60, curso);
+		disciplinaService.inserir(disciplina);
+		disciplinaService.listar().forEach(System.out::println);
+		
+		alunoService.matricular(aluno.getId(),curso.getId());
+	}
+}
---
+++ 05-jpa/src/main/java/br/com/fiap/jpa/dao/impl/EnderecoDAOImpl.java
@@ -0,0 +1,21 @@
+package br.com.fiap.jpa.dao.impl;
+
+import br.com.fiap.jpa.entity.Endereco;
+
+public class EnderecoDAOImpl extends HibernateGenericDAO<Endereco, Long> {
+
+	private static EnderecoDAOImpl instance;
+
+	private EnderecoDAOImpl() {
+		super(Endereco.class);
+	}
+
+	public static EnderecoDAOImpl getInstance() {
+
+		if (instance == null) {
+			instance = new EnderecoDAOImpl();
+		}
+
+		return instance;
+	}
+}
