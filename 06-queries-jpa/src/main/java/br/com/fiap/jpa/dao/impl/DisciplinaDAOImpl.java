b71e779636ac8f5b89311549d53fbac5acee6201
Logon Aluno <logonrmlocal@fiap.com.br>
2022-05-02 09:42:59 -0300

    Consultando com Query

---
+++ 06-queries-jpa/src/main/java/br/com/fiap/jpa/dao/impl/DisciplinaDAOImpl.java
@@ -0,0 +1,40 @@
+package br.com.fiap.jpa.dao.impl;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import br.com.fiap.jpa.entity.Disciplina;
+
+public class DisciplinaDAOImpl extends HibernateGenericDAO<Disciplina, Long> {
+
+	private static DisciplinaDAOImpl instance;
+	
+	private DisciplinaDAOImpl() {
+		super(Disciplina.class);
+	}
+	
+	public static DisciplinaDAOImpl getInstance() {
+		
+		if (instance == null) {
+			instance = new DisciplinaDAOImpl();
+		}
+		
+		return instance;
+	}
+	
+	public List<Disciplina> listarPorCurso(Long idCurso, EntityManager entityManager) {
+		String jpql = "SELECT d FROM Disciplina d WHERE d.curso.id = :idCurso";
+		TypedQuery<Disciplina> consulta = entityManager.createQuery(jpql, Disciplina.class);
+		consulta.setParameter("idCurso", idCurso);
+		
+		return consulta.getResultList();
+	}
+	public List<Disciplina> listar(EntityManager entityManager){
+		String jpql = "SELECT d FROM Disciplina d INNER JOIN FETCH d.curso";
+		TypedQuery<Disciplina> consulta = entityManager.createQuery(jpql,Disciplina.class);
+		return consulta.getResultList();
+	}
+
+}
